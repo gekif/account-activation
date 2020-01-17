786ac923cf732556b7920ea3ab0b8fa566b92454
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2020-01-17 20:17:39 +0700

    1. Introduction: 5. Database connection, migrations and models

--- database/migrations/2020_01_17_131353_create_activation_codes_table.php
+++ database/migrations/2020_01_17_131353_create_activation_codes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateActivationCodesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('activation_codes', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->boolean('active')->default(false);
-            $table->rememberToken();
+            $table->integer('user_id')->unsigned()->index();
+            $table->string('code');
             $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+
         });
     }
 
@@ -31,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('activation_codes');
     }
 }
