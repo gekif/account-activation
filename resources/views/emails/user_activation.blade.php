6da58d31bba619d95d7b3bb82287f8797e9fa6db
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2020-01-17 21:26:54 +0700

    3. Email setup: 4. Creating the controllers and routes

--- resources/views/emails/user_activation.blade.php
+++ resources/views/emails/user_activation.blade.php
@@ -1,3 +1,9 @@
-<h1>Hi, please click the link to activate your account <a href="{{ route('user.activation', $code) }}">
-        {{ route('user.activation', $code) }}
-    </a></h1>
+{{-- Using Send--}}
+{{--<h1>Hi, please click the link to activate your account <a href="{{ route('user.activation', $code) }}">--}}
+        {{--{{ route('user.activation', $code) }}--}}
+{{--</a></h1>--}}
+
+{{-- Using Queue--}}
+<h1>Hi, please click the link to activate your account <a href="{{ $url }}">
+        {{ $url }}
+</a></h1>
