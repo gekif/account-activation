1bc1047e31682a7a3408b644b23439ff135735d7
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2020-01-17 20:20:57 +0700

    1. Introduction: 6. Relations and fillable setup

--- app/ActivationCode.php
+++ app/ActivationCode.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class ActivationCode extends Model
 {
-    //
+    protected $fillable = ['code'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'active'
     ];
 
     /**
@@ -26,4 +26,20 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+
+    public function userActivationCode()
+    {
+        return $this->hasOne(ActivationCode::class);
+    }
+
+
+    public function userIsActivated()
+    {
+        if ($this->active) {
+            return true;
+        }
+
+        return false;
+    }
 }
